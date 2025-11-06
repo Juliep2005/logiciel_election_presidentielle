25c7fddf806c0a0019b33771db590f50b9f91b3b
Julie <juliettemvogo72@gmail.com>
2025-11-06 14:57:39 +0100

    Suppression du dossier indésirable

--- README.md
+++
@@ -1,2 +0,0 @@
-# logiciel_election_presidentielle
-Logiciel en C simulant le déroulement d'une élection présidentielle avec saisie des candidats, comptage des votes et affichage des résultats.
--- interface.h
+++
@@ -1,9 +0,0 @@
-#ifndef INTERFACE_H
-#define INTERFACE_H
-
-#include <windows.h>
-
-// Fonction principale pour démarrer l’interface
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int);
-
-#endif
